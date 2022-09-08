d944fd992a1509a2d5c0f4e2747c37c96cce6bac
zhiyongcao <zhiyongcao@emotibot.com>
2022-09-08 15:42:32 +0800

    1111

---
+++ Source/UtopiaLand/src/com/caozeal/leetcode/CanBeEqual.java
@@ -0,0 +1,77 @@
+package com.caozeal.leetcode;
+
+public class CanBeEqual {
+
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int max = 0;
+        for (int i : target) {
+            max = Math.max(max, i);
+        }
+        int[] temp = new int[max+1];
+        for (int i : target) {
+            temp[i] += 1;
+        }
+        for (int i : arr) {
+            if(i > max || temp[i] == 0){
+                return false;
+            }
+            temp[i] -= 1;
+        }
+        return true;
+    }
+
+    public boolean canBeEqual1(int[] target, int[] arr) {
+        int[] temp = new int[1001];
+        for (int i : target) {
+            temp[i] += 1;
+        }
+        for (int i : arr) {
+            if(temp[i] == 0){
+                return false;
+            }
+            temp[i] -= 1;
+        }
+        return true;
+    }
+
+    public boolean canBeEqual2(int[] target, int[] arr) {
+        short[] temp = new short[1001];
+        for (int i : target) {
+            temp[i]++;
+        }
+        for (int i : arr) {
+            if(temp[i]-- == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean canBeEqual3(int[] target, int[] arr) {
+        if(target.length > (1 << 7) -1){
+            short[] temp = new short[1001];
+            for (int i : target) {
+                temp[i] += 1;
+            }
+            for (int i : arr) {
+                if(temp[i] == 0){
+                    return false;
+                }
+                temp[i] -= 1;
+            }
+            return true;
+        }
+        byte[] temp = new byte[1001];
+        for (int i : target) {
+            temp[i] += 1;
+        }
+        for (int i : arr) {
+            if(temp[i] == 0){
+                return false;
+            }
+            temp[i] -= 1;
+        }
+        return true;
+    }
+
+}
---
+++ Source/UtopiaLand/src/com/caozeal/leetcode/ReverseList.java
@@ -0,0 +1,38 @@
+package com.caozeal.leetcode;
+
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode process;
+        head.next = null;
+        while (next != null) {
+            process = next.next;
+            next.next = head;
+            head = next;
+            next = process;
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
