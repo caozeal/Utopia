a77fb7dcf6d74461196edbf3ca566bed1dc893fb
zhiyongcao <zhiyongcao@emotibot.com>
2022-09-13 10:45:08 +0800

    1111

---
+++ Source/UtopiaLand/src/com/caozeal/leetcode/MinOperations.java
@@ -0,0 +1,21 @@
+package com.caozeal.leetcode;
+
+public class MinOperations {
+
+    public int minOperations(String[] logs) {
+        int deep = 0;
+        for (String log : logs) {
+            if(log.charAt(1) == '.'){
+                if(deep > 0){
+                    deep--;
+                }
+                continue;
+            }
+            if(log.charAt(0) == '.'){
+                continue;
+            }
+            deep++;
+        }
+        return deep;
+    }
+}
