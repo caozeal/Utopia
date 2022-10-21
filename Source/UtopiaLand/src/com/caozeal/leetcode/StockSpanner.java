06c98cc8caa01456877910ad488a7d5fa7d8f89b
zhiyongcao <zhiyongcao@emotibot.com>
2022-10-21 18:55:23 +0800

    add

---
+++ Source/UtopiaLand/src/com/caozeal/leetcode/StockSpanner.java
@@ -0,0 +1,49 @@
+package com.caozeal.leetcode;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockSpanner {
+
+    public static void main(String[] args){
+      StockSpanner sp = new StockSpanner();
+      sp.next(31);
+      sp.next(41);
+      sp.next(48);
+      sp.next(59);
+      sp.next(79);
+    }
+
+    private List<Pair> values;
+    public StockSpanner() {
+        values = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        int i = values.size()-1;
+        Pair pair = new Pair();
+        pair.val = price;
+        pair.nums = 1;
+        values.add(pair);
+        while (i >= 0){
+            Pair value = values.get(i);
+            Integer val = value.val;
+            Integer nums = value.nums;
+            if(val > price){
+                break;
+            }
+            pair.nums = pair.nums + value.nums;
+            i -= nums;
+        }
+        System.out.println(pair.nums);
+        return pair.nums;
+    }
+
+    static class Pair{
+        private Integer val;
+
+        private Integer nums;
+    }
+}
